6f686f19eb034dd85ade2afd0f95091a3e9c0d6f
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-04-05 20:09:01 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+// 1
+function distanceFromHqInBlocks(block){
+    if (block > 42) {
+        return block - 42;
+    } else {
+        return 42 - block
+    }
+}
+
+// 2
+function distanceFromHqInFeet(block){
+   return distanceFromHqInBlocks(block) * 264
+}
+
+// 3
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+      return (destination - start) * 264;
+    } else {
+      return (start - destination) * 264;
+    }
+  }
+  
+// 4
+  function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
